58fbdd5038b125d97563b8282b913ed5e40bdb9b
Raul Quintero <raulquintero13@gmail.com>
2022-12-22 16:33:56 -0800

    examples of pipes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,15 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'ang-proyecto012';
+  nombre = 'juan perez';
+  saldo = 100.50;
+  porcentaje = 0.23;
+  dias = ['domingo', 'lunes' , 'martes', 'miercoles', 'jueves', 'viernes', 'sabado', 'domingo'];
+  articulos = [
+    {codigo: 1, descripcion: "papas", precio: 10.01},
+    {codigo: 2, descripcion: "calabazas", precio: 11.01},
+    {codigo: 3, descripcion: "zanahorias", precio: 12},
+  ];
+  fechaActual = new Date();
+  
 }
